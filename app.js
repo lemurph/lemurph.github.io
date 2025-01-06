64ad3623de7a09b8211e631d360762312ab72124
lemurph <loganmurphy@logans-mbp.home>
2025-01-06 14:16:45 -0400

    Added project videos and CRT filter

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const logo_text = "lemurph";
 let lemurphHovered = false;
 let particles = [];
 
-const canvas = document.getElementById("mainCanvas");
-canvas.width  = window.innerWidth;
-canvas.height = window.innerHeight;
-const canvasContext = canvas.getContext("2d");
-canvasContext.font = "21px DOS";
+// const canvas = document.getElementById("mainCanvas");
+// canvas.width  = window.innerWidth;
+// canvas.height = window.innerHeight;
+// const canvasContext = canvas.getContext("2d");
+// canvasContext.font = "21px DOS";
 
 
 $(document).ready(function(){
@@ -30,50 +30,54 @@ $(document).ready(function(){
 
 }); 
 
-document.addEventListener("mousemove", function(event) {
-  mouseX = event.pageX;
-  mouseY = event.pageY;
-  particleText = bits.charAt(Math.floor(Math.random() * bits.length));
-  var particle = new Particle(mouseX, mouseY, particleText);
-  particle.draw();
-  particles.push(particle);
-});
+// POINTER TRAIL
+//
+// document.addEventListener("mousemove", function(event) {
+//   mouseX = event.pageX;
+//   mouseY = event.pageY;
+//   particleText = bits.charAt(Math.floor(Math.random() * bits.length));
+//   var particle = new Particle(mouseX, mouseY, particleText);
+//   particle.draw();
+//   particles.push(particle);
+// });
 
-class Particle {
-  constructor(posX, posY, text) {
-    this.posX = posX;
-    this.posY = posY
-    this.text = text
-  }
+// class Particle {
+//   constructor(posX, posY, text) {
+//     this.posX = posX;
+//     this.posY = posY
+//     this.text = text
+//   }
   
-  draw() {
-    canvasContext.fillStyle = "white";
-    canvasContext.fillText(this.text, this.posX, this.posY);
-  }
+//   draw() {
+//     canvasContext.fillStyle = "white";
+//     canvasContext.fillText(this.text, this.posX, this.posY);
+//   }
   
-  removeParticle() {
-    console.log("I am being deleted:" + this.text + "at pos " + this.posX + " " + this.posY)
-    canvasContext.fillStyle = "black";
-    canvasContext.fillRect(this.posX-15, this.posY-15, 30, 30);
-    particles.splice(particles.indexOf(this), 1);
-  }
+//   removeParticle() {
+//     canvasContext.fillStyle = "black";
+//     canvasContext.fillRect(this.posX-15, this.posY-15, 30, 30);
+//     particles.splice(particles.indexOf(this), 1);
+//   }
 
 
-}
+// }
+
+// setInterval(function() {
+//   if (particles.length > 0){
+//     particles[0].removeParticle();
+//   }
+// }, 9);
 
 setInterval(function() {
-  if (particles.length > 0){
-    particles[0].removeParticle();
-  }
-}, 9);
-
-setInterval(function() {
-  if (!lemurphHovered) {
+  if (lemurphHovered) {
     randomlogo_text = "";
-  for (let i=logo_text.length; i > 0; i--) {
-    randomlogo_text += characters.charAt(Math.floor(Math.random() * characters.length));
+    for (let i=logo_text.length; i > 0; i--) {
+      randomlogo_text += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    $("#lemurph").text(randomlogo_text);
   }
-  $("#lemurph").text(randomlogo_text);
+  else {
+    $("#lemurph").text("lemurph");
   }
 }, 100);
 
